83901a34683f87e5a66482d0f73c9a9c11abdacd
Abel <abelquezada01@hotmail.com>
2026-04-23 20:21:38 -0400

    Use UnitOfWork, remove WeatherForecast sample
    Replace individual repository registrations with a scoped UnitOfWork, add new _UnitOfWork.cs in Infrastructure/Repositories, and simplify Program.cs (remove unused using, remove UseAuthentication and tidy DI). Remove the WeatherForecast controller and model (sample/template code) and add Controllers folder entry to SGL.API.csproj. Build/artifact (bin/obj) files updated as part of the rebuild.

---
+++ Sistema de gestion de lavanderia/SGL.Infrastructure/Repositories/_UnitOfWork.cs
@@ -0,0 +1,27 @@
+﻿using SGL.Infrastructure.Data;
+
+namespace SGL.Infrastructure.Repositories;
+
+public class UnitOfWork
+{
+    private readonly SglDbContext _context;
+
+    public ClienteRepository Clientes { get; }
+    public EmpleadoRepository Empleados { get; }
+    public EntregaRepository Entregas { get; }
+    public FacturaRepository Facturas { get; }
+    public LoteRepository Lotes { get; }
+
+    public UnitOfWork(SglDbContext context)
+    {
+        _context = context;
+        Clientes  = new ClienteRepository(context);
+        Empleados = new EmpleadoRepository(context);
+        Entregas  = new EntregaRepository(context);
+        Facturas  = new FacturaRepository(context);
+        Lotes     = new LoteRepository(context);
+    }
+
+    public async Task CompleteAsync()
+        => await _context.SaveChangesAsync();
+}
